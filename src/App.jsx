8855c88af7cc1672bd9e561eb6cbcabdd56899cc
183Natasha <168049595+183Natasha@users.noreply.github.com>
2025-05-15 14:47:56 +0500

    start

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Button  from './components/Button/Button';
+import JournalItem from './components/JournalItem/JournalItem';
+
+function App() {
+
+	return (
+		<>
+			<h1>Заголовок</h1>
+			<p>Какой-то текст</p>
+			<Button/>
+			<JournalItem/>
+		</>
+	);
+}
+
+export default App;
