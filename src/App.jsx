506fd40345539c9ab3b800ae07d917059b76c9fe
183Natasha <168049595+183Natasha@users.noreply.github.com>
2025-05-18 17:03:43 +0500

    date

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,33 @@ import Button  from './components/Button/Button';
 import JournalItem from './components/JournalItem/JournalItem';
 
 function App() {
-
+	const data= [
+		{
+			title: 'Подготовка к обновлению курсов',
+	    date: new Date(),
+	    text: 'Сегодня провёл весь день за...'
+		},
+		{
+			title: 'Поход в годы',
+	    date: new Date(),
+	    text: 'Думал, что очень много времени'
+		}
+	];
 	return (
 		<>
 			<h1>Заголовок</h1>
 			<p>Какой-то текст</p>
 			<Button/>
-			<JournalItem/>
+			<JournalItem
+				title={data[0].title}
+				date={data[0].date}
+				text={data[0].text}
+			/>
+			<JournalItem
+				title={data[1].title}
+				date={data[1].date}
+				text={data[1].text}
+			/>
 		</>
 	);
 }
