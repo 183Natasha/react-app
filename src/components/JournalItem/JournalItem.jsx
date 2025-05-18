620c113c22db9dd579be4acaa3028463a647a71c
183Natasha <168049595+183Natasha@users.noreply.github.com>
2025-05-18 16:36:23 +0500

    динамические данные

--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -2,12 +2,16 @@ import './JournalItem.css';
 
 function JournalItem() {
 
+	const title = 'Подготовка к обновлению курсов';
+	const date = new Date();
+	const text = 'Подготовка к обновлению курсов';
+
 	return (
 		<div className='journal-item' >
-			<h2 className='journal-item__header'>1</h2>
+			<h2 className='journal-item__header'>{title}</h2>
 			<h2 className='journal-item__body'>
-				<div className='journal-item__date'>2</div>
-				<div className='journal-item__text'>3</div>
+				<div className='journal-item__date'>{date.toString()}</div>
+				<div className='journal-item__text'>{text}</div>
 			</h2>
 		</div>
 	);
