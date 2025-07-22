6533bf99d8dc559c6e1157f6c8068ab5cf5ecc6e
183Natasha <168049595+183Natasha@users.noreply.github.com>
2025-07-22 22:33:39 +0500

    динамическая стилизация через style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css';
 import Button  from './components/Button/Button';
-import JournalItem from './components/JournalItem/JournalItem';
-import CardButton from './components/CardButton/CardButton';
+
 import LeftPanel from './layouts/LeftPanel/leftPanel';
 import Body from './layouts/Body/Body';
 import Header from './components/Header/Header';
@@ -11,16 +10,16 @@ import JournalForm from './components/JournalForm/JournalForm';
 import { useState } from 'react';
 
 const INITIAL_DATA= [
-	{	id:1,
-		title: 'Подготовка к обновлению курсов',
-		date: new Date(),
-		text: 'Сегодня провёл весь день за...'
-	},
-	{	id:2,
-		title: 'Поход в годы',
-		date: new Date(),
-		text: 'Думал, что очень много времени'
-	}
+	// {	id:1,
+	// 	title: 'Подготовка к обновлению курсов',
+	// 	date: new Date(),
+	// 	text: 'Сегодня провёл весь день за...'
+	// },
+	// {	id:2,
+	// 	title: 'Поход в годы',
+	// 	date: new Date(),
+	// 	text: 'Думал, что очень много времени'
+	// }
 ];
 
 
@@ -32,36 +31,18 @@ function App() {
 			title: item.title,
 			date: new Date(item.date),
 			text: item.text,
-			id: Math.max(...oldItem.map(i=>i.id))+1
+			id: oldItem.length > 0 ? Math.max(...oldItem.map(i=>i.id))+1 : 1
 		}]);
 	};
 
-	const sortItems = (a,b) =>{
-		if (a.date>b.date){
-			return 1;
-		} else{
-			return -1;
-		}
-	};
 	
-
 	return (
 		<div className='app'>
 			<LeftPanel>
 				<Header/>
 				<JournalAddButton/>
-				<JournalList>
-					{items.sort(sortItems).map(el => (
-						<CardButton key={el.id}>
-							<JournalItem
-								title={el.title}
-								date={el.date}
-								text={el.text}
-							/>
-						</CardButton>
-					)
-					)}
-				</JournalList>
+				<JournalList items = {items} />
+
 			</LeftPanel>
 
 			<Body>
--- src/components/JournalList/JournalList.jsx
+++ src/components/JournalList/JournalList.jsx
@@ -1,12 +1,30 @@
 import './JournalList.css';
+import JournalItem from '../JournalItem/JournalItem';
+import CardButton from '../CardButton/CardButton';
 
-function JournalList({children}) {
+function JournalList({items}) {
+	if (items.length == 0){
+		return <p>Записей пока нет, добавьте первую</p>;
+	}
+	const sortItems = (a,b) =>{
+		if (a.date>b.date){
+			return 1;
+		} else{
+			return -1;
+		}
+	};
+	
+	return <> {items.sort(sortItems).map(el => (
+		<CardButton key={el.id}>
+			<JournalItem
+				title={el.title}
+				date={el.date}
+				text={el.text}
+			/>
+		</CardButton>
+	))
+	}</>;
 
-	return (
-		<div className='journal-list'>
-			{children}
-		</div>
-	);
 }
 
 export default JournalList;
