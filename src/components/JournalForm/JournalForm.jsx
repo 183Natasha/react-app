857730578c3a03a82cb65b0ca9cc3036f0ccf9f8
183Natasha <168049595+183Natasha@users.noreply.github.com>
2025-08-03 10:23:11 +0500

    очистка формы

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,82 +1,97 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Button from '../Button/Button';
 import styles from './JournalForm.module.css';
 import cn from 'classnames';
 
 
-function JournalForm({onSubmit}) {
-	const [formValidState, setFormValidState] = useState({
-		title: true,
-		post: true,
-		date: true
-	});
+let INITIAL_STATE = {
+	title: true,
+	post: true,
+	date: true
+};
+
+function JournalForm({ onSubmit }) {
+	const [formValidState, setFormValidState] = useState(INITIAL_STATE);
+
+	useEffect(() => {
+		let timerId;
+		if (!formValidState.date || !formValidState.post || !formValidState.title) {
+			timerId = setTimeout(() => {
+				console.log('Очистка');
+				setFormValidState(INITIAL_STATE);
+			}, 2000);
+			return () => {
+				clearTimeout(timerId);
+			};
+		}
+	}, [formValidState]);
 
 	const addJournalItem = (e) => {
 		e.preventDefault();
 		const formData = new FormData(e.target);
 		const formProps = Object.fromEntries(formData);
 		let isFormValid = true;
-		if (!formProps.title?.trim().length){
-			setFormValidState(state => ({...state, title: false}));
+		if (!formProps.title?.trim().length) {
+			setFormValidState(state => ({ ...state, title: false }));
 			isFormValid = false;
-		}else{
-			setFormValidState(state => ({...state, title: true}));
+		} else {
+			setFormValidState(state => ({ ...state, title: true }));
 		}
-		if (!formProps.post?.trim().length){
-			setFormValidState(state => ({...state, post: false}));
+		if (!formProps.post?.trim().length) {
+			setFormValidState(state => ({ ...state, post: false }));
 			isFormValid = false;
-		} else{
-			setFormValidState(state => ({...state, post: true}));
+		} else {
+			setFormValidState(state => ({ ...state, post: true }));
 		}
-		if (!formProps.date){
-			setFormValidState(state => ({...state, date: false}));
+		if (!formProps.date) {
+			setFormValidState(state => ({ ...state, date: false }));
 			isFormValid = false;
-		}else{
-			setFormValidState(state => ({...state, date: true}));
+		} else {
+			setFormValidState(state => ({ ...state, date: true }));
 		}
-		if (!isFormValid){
+		if (!isFormValid) {
 			return;
 		}
-		onSubmit(formProps );
+		onSubmit(formProps);
 	};
 
 	return (
-        
-		<form className={styles['journal-form' ]}onSubmit={addJournalItem}>
+
+		<form className={styles['journal-form']} onSubmit={addJournalItem}>
 			<div>
 				<input type="text" name="title" id="" className={cn(styles['input-title'], {
-					[styles['invalid'] ]: !formValidState.title
+					[styles['invalid']]: !formValidState.title
 				}
 				)} />
 			</div>
 
 			<div className={styles['form-row']}>
-				<label htmlFor="date"className={styles['form-label']}>
+				<label htmlFor="date" className={styles['form-label']}>
 					<img src="/calendar.svg" alt="Иконка календаря" />
 					<span>Дата</span>
 				</label>
 				<input type="date" name="date" id="date" className={cn(styles['input'], {
-					[styles['invalid'] ]: !formValidState.date
+					[styles['invalid']]: !formValidState.date
 				}
 				)} />
 			</div>
 
 			<div className={styles['form-row']}>
-				<label htmlFor="tag"className={styles['form-label']}>
+				<label htmlFor="tag" className={styles['form-label']}>
 					<img src="/folder.svg" alt="Иконка папки" />
 					<span>Метки</span>
 				</label>
 				<input type="text" name="tag" id='tag' className={styles['input']} />
 			</div>
-			
-			
-			
-			<textarea name="post" id="" cols={30} rows={10}  className={cn(styles['input'], {
-				[styles['invalid'] ]: !formValidState.post
+
+
+
+			<textarea name="post" id="" cols={30} rows={10} className={cn(styles['input'], {
+				[styles['invalid']]: !formValidState.post
 			}
 			)} ></textarea>
-			<Button text = 'Сохранить' ></Button>
-			
+			<Button text='Сохранить' ></Button>
+
 		</form>
 	);
 }
